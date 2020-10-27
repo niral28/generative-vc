5515184f66721242e35c60fc451c56c9950a84a9
Niral Shah <niralshah@Nirals-MBP-2.fios-router.home>
2020-10-26 22:37:09 -0400

    initial commit

---
+++ solver_encoder.py
@@ -0,0 +1,132 @@
+from model_vc import Generator
+import torch
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+import time
+import datetime
+
+
+class Solver(object):
+
+    def __init__(self, vcc_loader, config):
+        """Initialize configurations."""
+
+        # Data loader.
+        self.vcc_loader = vcc_loader
+
+        # Model configurations.
+        self.lambda_cd = config.lambda_cd
+        self.dim_neck = config.dim_neck
+        self.dim_emb = config.dim_emb
+        self.dim_pre = config.dim_pre
+        self.freq = config.freq
+
+        # Training configurations.
+        self.batch_size = config.batch_size
+        self.num_iters = config.num_iters
+        
+        # Miscellaneous.
+        self.use_cuda = torch.cuda.is_available()
+        self.device = torch.device('cuda:0' if self.use_cuda else 'cpu')
+        self.log_step = config.log_step
+
+        # Build the model and tensorboard.
+        self.build_model()
+
+            
+    def build_model(self):
+        
+        self.G = Generator(self.dim_neck, self.dim_emb, self.dim_pre, self.freq)        
+        
+        self.g_optimizer = torch.optim.Adam(self.G.parameters(), 0.0001)
+        
+        self.G.to(self.device)
+        
+
+    def reset_grad(self):
+        """Reset the gradient buffers."""
+        self.g_optimizer.zero_grad()
+      
+    
+    #=====================================================================================================================================#
+    
+    
+                
+    def train(self):
+        # Set data loader.
+        data_loader = self.vcc_loader
+        writer = SummaryWriter()
+        # Print logs in specified order
+        keys = ['G/loss_id','G/loss_id_psnt','G/loss_cd']
+            
+        # Start training.
+        print('Start training...')
+        start_time = time.time()
+        for i in range(self.num_iters):
+
+            # =================================================================================== #
+            #                             1. Preprocess input data                                #
+            # =================================================================================== #
+
+            # Fetch data.
+            try:
+                x_real, emb_org = next(data_iter)
+            except:
+                data_iter = iter(data_loader)
+                x_real, emb_org = next(data_iter)
+            
+            
+            x_real = x_real.to(self.device) 
+            emb_org = emb_org.to(self.device) 
+                        
+       
+            # =================================================================================== #
+            #                               2. Train the generator                                #
+            # =================================================================================== #
+            
+            self.G = self.G.train()
+                        
+            # Identity mapping loss
+            x_identic, x_identic_psnt, code_real = self.G(x_real, emb_org, emb_org)
+            g_loss_id = F.mse_loss(x_real, x_identic)   
+            g_loss_id_psnt = F.mse_loss(x_real, x_identic_psnt)   
+            
+            # Code semantic loss.
+            code_reconst = self.G(x_identic_psnt, emb_org, None)
+            g_loss_cd = F.l1_loss(code_real, code_reconst)
+
+
+            # Backward and optimize.
+            g_loss = g_loss_id + g_loss_id_psnt + self.lambda_cd * g_loss_cd
+            self.reset_grad()
+            g_loss.backward()
+            self.g_optimizer.step()
+
+            # Logging.
+            loss = {}
+            loss['G/loss_id'] = g_loss_id.item()
+            loss['G/loss_id_psnt'] = g_loss_id_psnt.item()
+            loss['G/loss_cd'] = g_loss_cd.item()
+            writer.add_scalar('G/loss_id', loss['G/loss_id'], i)
+            writer.add_scalar('G/loss_id_psnt', loss['G/loss_id_psnt'], i)
+            writer.add_scalar('G/loss_cd', loss['G/loss_cd'], i)
+            
+            # =================================================================================== #
+            #                                 4. Miscellaneous                                    #
+            # =================================================================================== #
+
+            # Print out training information.
+            if (i+1) % self.log_step == 0:
+                et = time.time() - start_time
+                et = str(datetime.timedelta(seconds=et))[:-7]
+                log = "Elapsed [{}], Iteration [{}/{}]".format(et, i+1, self.num_iters)
+                for tag in keys:
+                    log += ", {}: {:.4f}".format(tag, loss[tag])
+                print(log)
+            torch.save(self.G.state_dict(), 'model_inference_state_dict')
+            torch.save(self.G, 'model_train.pt')
+
+    
+    
+
+    
